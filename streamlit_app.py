c301004abe9b1fdcaf24c11773654a505a49ebd1
Niagara-ADR <adr_niagara_test@outlook.com>
2024-08-05 20:41:29 +0000

    Vector storage version

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,88 +7,13 @@ import json
 import time
 from dateutil.relativedelta import relativedelta
 import re
+import os
 
 st.title("ARCH Ticket Assistant")
 
 openai_api_key = st.secrets.openai_api_key
 
 client = OpenAI(api_key=openai_api_key)
-
-def get_ticket_details(start_date=None, end_date=None, date_string=None):
-    if not start_date and not end_date:
-        if not date_string:
-            date_string = "recent"
-        today = datetime.today()
-        if date_string == "past month":
-            start_date = today.replace(day=1)
-            end_date = today
-        elif date_string == "last year":
-            start_date = today.replace(year=today.year - 1, month=1, day=1)
-            end_date = today.replace(year=today.year - 1, month=12, day=31)
-        elif date_string == "this year" or date_string == "past year":
-            start_date = today.replace(month=1, day=1)
-            end_date = today
-        elif date_string == "recent" or date_string == "past week" or date_string == "this year":
-            start_date = today - timedelta(days=7)
-            end_date = today
-        else:
-            match_days = re.match(r'last (\d+) days', date_string)
-            match_weeks = re.match(r'last (\d+) weeks', date_string)
-            match_months = re.match(r'last (\d+) months', date_string)
-            
-            if match_days:
-                days = int(match_days.group(1))
-                start_date = today - timedelta(days=days)
-                end_date = today
-            elif match_weeks:
-                weeks = int(match_weeks.group(1))
-                start_date = today - timedelta(weeks=weeks)
-                end_date = today
-            elif match_months:
-                months = int(match_months.group(1))
-                start_date = today - relativedelta(months=months)
-                end_date = today
-            else:
-                start_date = today - timedelta(days=7)
-                end_date = today
-        start_date = start_date.strftime('%Y-%m-%d')
-        end_date = end_date.strftime('%Y-%m-%d')
-
-
-    url = st.secrets.token_url
-    payload = {
-        "grant_type": "client_credentials",
-        "client_id": st.secrets.client_id,
-        "client_secret": st.secrets.client_secret,
-    }
-
-    response = requests.post(url, data=payload)
-
-    access_token = response.json().get('access_token')
-
-    headers = {
-            'Authorization': 'Bearer:' + access_token,
-            'Content-Type': 'application/json'
-        }
-    if not start_date or datetime.strptime(start_date, "%Y-%m-%d").date() > datetime.now().date():
-        start_date = (datetime.now().date() - timedelta(days=7)).strftime('%Y-%m-%d')
-    if not end_date or datetime.strptime(end_date, "%Y-%m-%d").date() > datetime.now().date():
-        end_date=(datetime.now().date()).strftime('%Y-%m-%d')
-    print(start_date)
-    print(end_date)
-    response = requests.get('https://uat-archapi.niagarawater.com/api/tickets/getAllTicketDetails?usecase_id=["60cb784aafe4530011138ca9"]&start_date={}&end_date={}'.format(start_date, end_date), headers = headers)
-    json = response.json()
-    df = pd.DataFrame(json['response'])
-    try:
-        # columns: plant code, full name of plant, possible causes
-        df = df.drop('_id', axis=1)
-        df = df.drop('lastest_Message', axis=1)
-        df.sort_values(by='ticket_creation_date', ascending=False, inplace=True)
-        #df = df.head(50)
-    except:
-      pass
-    print(df)
-    return df.to_json(orient='index')
 
 class AssistantManager:
     assistant_id = st.secrets.assistant_id
@@ -105,23 +30,100 @@ class AssistantManager:
         self.assistant = self.client.beta.assistants.retrieve(
             assistant_id=AssistantManager.assistant_id
         )
-    
-    def upload_file(self, text):
-        def encode_texts(texts):
-            response = self.client.embeddings.create(
-                input=texts,
-                engine="text-embedding-3-small"
-            )
-            return response.data[0].embedding
-    
-        embedding = encode_texts(text)
-        new_document = {
-            "id": f"ticket_information_{time.time()}",
-            "vector": embedding,
-            "metadata": {
-                "text": text
+
+    def get_ticket_details(self, start_date=None, end_date=None, date_string=None):
+        if not start_date and not end_date:
+            if not date_string:
+                date_string = "recent"
+            today = datetime.today()
+            if date_string == "past month":
+                start_date = today.replace(day=1)
+                end_date = today
+            elif date_string == "last year":
+                start_date = today.replace(year=today.year - 1, month=1, day=1)
+                end_date = today.replace(year=today.year - 1, month=12, day=31)
+            elif date_string == "this year" or date_string == "past year":
+                start_date = today.replace(month=1, day=1)
+                end_date = today
+            elif date_string == "recent" or date_string == "past week" or date_string == "this year":
+                start_date = today - timedelta(days=7)
+                end_date = today
+            else:
+                match_days = re.match(r'last (\d+) days', date_string)
+                match_weeks = re.match(r'last (\d+) weeks', date_string)
+                match_months = re.match(r'last (\d+) months', date_string)
+                
+                if match_days:
+                    days = int(match_days.group(1))
+                    start_date = today - timedelta(days=days)
+                    end_date = today
+                elif match_weeks:
+                    weeks = int(match_weeks.group(1))
+                    start_date = today - timedelta(weeks=weeks)
+                    end_date = today
+                elif match_months:
+                    months = int(match_months.group(1))
+                    start_date = today - relativedelta(months=months)
+                    end_date = today
+                else:
+                    start_date = today - timedelta(days=7)
+                    end_date = today
+            start_date = start_date.strftime('%Y-%m-%d')
+            end_date = end_date.strftime('%Y-%m-%d')
+
+
+        url = st.secrets.token_url
+        payload = {
+            "grant_type": "client_credentials",
+            "client_id": st.secrets.client_id,
+            "client_secret": st.secrets.client_secret,
+        }
+
+        response = requests.post(url, data=payload)
+
+        access_token = response.json().get('access_token')
+
+        headers = {
+                'Authorization': 'Bearer:' + access_token,
+                'Content-Type': 'application/json'
             }
-    }
+        if not start_date or datetime.strptime(start_date, "%Y-%m-%d").date() > datetime.now().date():
+            start_date = (datetime.now().date() - timedelta(days=7)).strftime('%Y-%m-%d')
+        if not end_date or datetime.strptime(end_date, "%Y-%m-%d").date() > datetime.now().date():
+            end_date=(datetime.now().date()).strftime('%Y-%m-%d')
+        print(start_date)
+        print(end_date)
+        response = requests.get('https://uat-archapi.niagarawater.com/api/tickets/getAllTicketDetails?usecase_id=["60cb784aafe4530011138ca9"]&start_date={}&end_date={}'.format(start_date, end_date), headers = headers)
+        json = response.json()
+        df = pd.DataFrame(json['response'])
+        try:
+            df = df.drop('_id', axis=1)
+            df = df.drop('lastest_Message', axis=1)
+            df = df.drop('comments', axis=1)
+            df.sort_values(by='ticket_creation_date', ascending=False, inplace=True)
+        except:
+            pass
+        print(df)
+        file_name = f'{start_date}_to_{end_date}'
+        with open(f'{file_name}.json', 'w') as f:
+            f.write(df.to_json(orient='index'))       
+        self.upload_file(f'{file_name}.json')
+        os.remove(f'{file_name}.json')
+        return df.to_json(orient='index')
+    
+    def upload_file(self, file):
+        try:
+            file_streams = [open(file, "rb")]
+
+            file_batch = client.beta.vector_stores.file_batches.upload_and_poll(
+                vector_store_id=AssistantManager.vector_store_id, files=file_streams
+            )
+            time.sleep(1)
+            print("UPLOADING FILE")
+            print(file_batch.status)
+            print(file_batch.file_counts)
+        except:
+            pass
 
     
     def create_thread(self):
@@ -166,12 +168,13 @@ class AssistantManager:
             func_name = action["function"]["name"]
             arguments = json.loads(action['function']['arguments'])
             if func_name == "get_ticket_details":
-                output = get_ticket_details(start_date=arguments.get("start_date"), end_date=arguments.get("end_date"), date_string=arguments.get("date_string"))
+                output = self.get_ticket_details(start_date=arguments.get("start_date"), end_date=arguments.get("end_date"), date_string=arguments.get("date_string"))
                 tool_outputs.append({"tool_call_id" : action["id"], "output" : output})
             else:
                 raise ValueError(f"Unknown function: {func_name}")
         
         print(tool_outputs)
+        self.upload_file(str(tool_outputs))
 
         self.client.beta.threads.runs.submit_tool_outputs(
             thread_id=self.thread.id, run_id=self.run.id, tool_outputs=tool_outputs
